8a1fa5eb7391f87ad158549cf4e0058f580e0220
cloneproduction <clone@cloneproduction.net>
2023-04-29 11:59:34 +0800

    Added Enum and Invert TFX

---
+++ src/TextureFX/InvertEnum.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+
+namespace Fuse.Common.TextureFX
+{
+    [Flags]
+    public enum InvertMode
+    {
+        [Description("RGB")]
+        RGB = 0,
+        [Description("Red")]
+        Red = 1,
+        [Description("Green")]
+        Green = 2,
+        [Description("RedGreen")]
+        RedGreen = 3,
+        [Description("RedBlue")]
+        RedBlue = 4,
+        [Description("GreenBlue")]
+        GreenBlue = 5,
+        [Description("Value")]
+        Value = 6,
+        [Description("Lightness")]
+        Lightness = 7,
+        [Description("Saturation")]
+        Saturation = 8,
+        [Description("Hue")]
+        Hue = 9,
+        [Description("Cyan")]
+        Cyan = 10,
+        [Description("Magenta")]
+        Magenta = 11,
+        [Description("Yellow")]
+        Yellow = 12,
+        [Description("Black")]
+        Black = 13,
+    }
+}
